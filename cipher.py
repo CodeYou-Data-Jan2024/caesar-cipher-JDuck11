207ad0c65bb69ac6f2471ac5a8beb3538b617fe3
jduck11 <jjhowa02@icloud.com>
2024-04-07 19:39:14 -0400

    cipher

--- cipher.py
+++ cipher.py
@@ -1,1 +1,19 @@
-# add your code here
+print("input phase: ")
+phrase = input()
+
+print("you enter:" +phrase)
+
+alphabet = "abcdefghijklmnopqrstuvwxyzabcde"
+newPhrase = " "
+
+for i in range(0, len(phrase)):
+    try:
+        char = phrase[i]
+        newChar = str(alphabet[alphabet.index(char)+5])
+        newPhrase += newChar
+    
+    except:
+        newChar = str(alphabet[0])
+        newPhrase += newChar
+
+print("the cipher phase is" + newPhrase)
